c1a75750cce5e049d252350ce38e11a5dffb686e
suzy-456 <76138077+suzy-456@users.noreply.github.com>
2021-01-11 18:20:05 -0800

    Update app.js

--- exercise1/app.js
+++ exercise1/app.js
@@ -1,10 +1,10 @@
 // Write your JS in here
 var pics = [
-	"imgs/kitty_bed.jpg",		//0
-	"imgs/kitty_basket.jpg", 	//1
+	"imgs/kitty_bed.JPG",		//0
+	"imgs/kitty_basket.JPG", 	//1
 	"imgs/kitty_laptop.jpg",	//2
 	"imgs/kitty_door.jpg",		//3
-	"imgs/kitty_sink.jpg",		//4
+	"imgs/kitty_sink.JPG",		//4
 	"imgs/kitty_wall.jpg"		//5
 								//6
 ];
